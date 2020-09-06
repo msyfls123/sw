6957277f95b92b09cbbcea69790afc4b3f6d4fd8
msyfls123 <msyfls123@gmail.com>
2020-09-06 23:51:26 +0800

    fix typo

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import Root from './root'
 
 import './app.styl'
 
-if ('serviceWorker' in navigator && process.env.NODE_MODE === 'production') {
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   navigator.serviceWorker.register('/sw/dist/sw.js', { scope: '/sw/dist/' }).then(() => {
     console.log('success register sw.js!')
   })
